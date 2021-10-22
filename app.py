e1ea06e64edb3942bbe7ee7a5894cb2f1472af36
OmarJ <omarjandali93@gmail.com>
2021-10-22 11:06:04 -0700

    completed the task manager. all crud functions working

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -16,7 +16,43 @@ class Todo(db.Model):
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-  return render_template('index.html')
+  if request.method == 'POST':
+    task_content = request.form['content'] 
+    new_task = Todo(content=task_content)
+    try:
+      db.session.add(new_task)
+      db.session.commit()
+      return redirect('/')
+    except:
+      return 'Issue adding your task'
+  else: 
+    tasks = Todo.query.all()
+    print(tasks)
+  return render_template('index.html', tasks=tasks)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+  task_to_delete = Todo.query.get_or_404(id)
+  try:
+    db.session.delete(task_to_delete)
+    db.session.commit()
+    return redirect('/')
+  except:
+    return 'There was an issue deleting the task'
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+  task = Todo.query.get_or_404(id)
+  if request.method == 'POST':
+    task.content = request.form['content']
+    try:
+      db.session.commit()
+      return redirect('/')
+    except:
+      return 'There was an issue updating your task'
+
+  else:
+    return render_template('update.html', task=task)
 
 if __name__ == '__main__':
   app.run(debug=True)
